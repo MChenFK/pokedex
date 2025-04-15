4b7fcbdbee7f589e5c4d2790323b6a0883f0970f
MChenFK <michaelchen1923@gmail.com>
2025-04-14 20:07:23 -0400

    added explore command

--- repl.go
+++ repl.go
@@ -27,11 +27,16 @@ func startRepl(cfg *config) {
 		}
 
 		firstWord := cleanedInput[0]
+		args := []string{}
+		if len(cleanedInput) > 1 {
+			args = cleanedInput[1:]
+		}
+
 		command, ok := getCommands()[firstWord]
 		if !ok {
 			fmt.Println("Unknown command")
 		} else {
-			err := command.callback(cfg)
+			err := command.callback(cfg, args...)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -50,7 +55,7 @@ func cleanInput(text string) []string {
 type cliCommand struct {
 	name		string
 	description	string
-	callback	func(*config) error
+	callback	func(*config, ...string) error
 }
 
 func getCommands() map[string]cliCommand {
@@ -69,6 +74,11 @@ func getCommands() map[string]cliCommand {
 			name:        "mapb",
 			description: "Displays the previous 20 location areas in the Pokemon world",
 			callback:    commandMapb,
+		},
+		"explore": {
+			name:        "explore <location_name>",
+			description: "Takes the name of a location area and displays a list of all the Pokemon located there",
+			callback:    commandExplore,
 		},
 		"exit": {
 			name:        "exit",
